a8e7643189f8ad3de2d573101deaba45efbacc98
´lrondan´ <lrondan1126@gmail.com>
2024-07-02 17:55:22 +0200

    del

---
+++ 0.5 PornHub models/bycountry.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import csv
+
+archivo = '0.5 PornHub models\Models by country.csv'
+with open(archivo,'r')as file:
+    datas = pd.read_csv(file)
+    df = pd.DataFrame(data=datas)
