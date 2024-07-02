f06386a6edbd896266626c9d53583b0dc2a1225a
´lrondan´ <lrondan1126@gmail.com>
2024-07-02 18:01:02 +0200

    firts

--- 0.5 PornHub models/bycountry.py
+++ 0.5 PornHub models/bycountry.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import csv
 
-archivo = '0.5 PornHub models\Models by country.csv'
+archivo = '0.5 PornHub models/Models by country.csv'
 with open(archivo,'r')as file:
     datas = pd.read_csv(file)
     df = pd.DataFrame(data=datas)
+
+countries = df['Countries'].value_counts()
+
+print(countries)
